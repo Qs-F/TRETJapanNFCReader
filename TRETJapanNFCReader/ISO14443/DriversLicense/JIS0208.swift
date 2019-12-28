081440fccb6543683042e03f516bd6f85f7ae297
Qs-F <qsf@de-liker.com>
2019-12-29 04:15:27 +0900

    Add conv.sh

---
+++ TRETJapanNFCReader/ISO14443/DriversLicense/JIS0208.swift
@@ -0,0 +1,9 @@
+//
+//  JIS0208.swift
+//  DriversLicenseReader
+//
+//  Created by CreatorQsF on 2019/12/29.
+//  Copyright © 2019 treastrain / Tanaka Ryoga. All rights reserved.
+//
+
+import Foundation
